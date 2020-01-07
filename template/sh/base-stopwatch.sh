8874fd8153e94fc33f9248d07cc872ea42d22536
mggggk <mggggk@gmail.com>
2020-01-07 22:29:06 +0900

    bashテンプレートにサブルーチンを追加

--- template/sh/base-stopwatch.sh
+++ template/sh/base-stopwatch.sh
@@ -3,12 +3,17 @@
 # Usage
 {{_cursor_}}
 
+# subroutine
+test () {
+  echo this is test $1 $2 $3
+}
 
 # Main script
 cd `dirname $0`
 start_time=`date +%s`
 
 ### 時間測定したい処理
+test 'arg1' 2 3
 sleep 3
 
 end_time=`date +%s`
