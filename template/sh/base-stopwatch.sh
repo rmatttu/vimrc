d7e0d08bbb23b320d023c1e6c897efb91ebbad84
mggggk <mggggk@gmail.com>
2021-01-25 04:20:32 +0900

    Update shell script template

--- template/sh/base-stopwatch.sh
+++ template/sh/base-stopwatch.sh
@@ -1,22 +1,30 @@
 #!/bin/sh
 
 # Usage
-{{_cursor_}}
+# {{_cursor_}}
 
-# subroutine
-test () {
+# set -e
+# set -u
+
+# Function definition
+f () {
   echo this is test $1 $2 $3
 }
 
 # Main script
-cd `dirname $0`
 start_time=`date +%s`
 
-### 時間測定したい処理
-test 'arg1' 2 3
+# Current directory
+cd `dirname $0`
+script_dir_path=$(dirname $(readlink -f $0))
+
+# Function
+f 'arg1' 2 3
 sleep 3
+
+# xargs
+find ./ -newermt "2021-01-01" -type f | xargs -I{} echo {}
 
 end_time=`date +%s`
 time=$((end_time - start_time))
 echo $time
-
