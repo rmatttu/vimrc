2d5eefd24a3dd34bec29c728f5da5477f5f33b7c
mggggk <mggggk@gmail.com>
2022-09-17 20:29:44 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,14 +3,34 @@
 set -e
 set -u
 
-cd `dirname $0`
-mkdir -p tmp
-mkdir -p local/vsnip
+now_text=`date +%Y-%m-%d_%H-%M-%S`
+old_dir=~/old-config-vim-$now_text
+
+# Backup if exists
+if [[ -e ~/.config/vim ]]; then
+	mkdir -p $old_dir
+	mv ~/.config/vim $old_dir
+fi
+if [[ -e ~/.vimrc ]]; then
+	mkdir -p $old_dir
+	mv ~/.vimrc $old_dir
+fi
+if [[ -e ~/.gvimrc ]]; then
+	mkdir -p $old_dir
+	mv ~/.gvimrc $old_dir
+fi
+
+(
+	cp -r ./ ~/.config/vim/
+  cd ~/.config/vim
+	mkdir -p tmp
+	mkdir -p local/vsnip
+)
 
 cat << EOT > ~/.vimrc
-source \$HOME/.vim/vimrc
+source \$HOME/.config/vim/vimrc
 EOT
 
 cat << EOT > ~/.gvimrc
-source \$HOME/.vim/gvimrc
+source \$HOME/.config/vim/gvimrc
 EOT
