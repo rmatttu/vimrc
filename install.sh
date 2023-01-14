70c6f9d2cc4a60eb52446ee5f95d6973d265eef6
mggggk <mggggk@gmail.com>
2023-01-14 15:26:43 +0900

    Fix shell script format

--- install.sh
+++ install.sh
@@ -3,34 +3,34 @@
 set -e
 set -u
 
-now_text=`date +%Y-%m-%d_%H-%M-%S`
+now_text=$(date +%Y-%m-%d_%H-%M-%S)
 old_dir=~/old-config-vim-$now_text
 
 # Backup if exists
 if [[ -e ~/.config/vim ]]; then
-	mkdir -p $old_dir
-	mv ~/.config/vim $old_dir
+	mkdir -p "$old_dir"
+	mv ~/.config/vim "$old_dir"
 fi
 if [[ -e ~/.vimrc ]]; then
-	mkdir -p $old_dir
-	mv ~/.vimrc $old_dir
+	mkdir -p "$old_dir"
+	mv ~/.vimrc "$old_dir"
 fi
 if [[ -e ~/.gvimrc ]]; then
-	mkdir -p $old_dir
-	mv ~/.gvimrc $old_dir
+	mkdir -p "$old_dir"
+	mv ~/.gvimrc "$old_dir"
 fi
 
 (
 	rsync -av --exclude='.git' ./ ~/.config/vim/
-  cd ~/.config/vim
+	cd ~/.config/vim
 	mkdir -p tmp
 	mkdir -p local/vsnip
 )
 
-cat << EOT > ~/.vimrc
+cat <<EOT >~/.vimrc
 source \$HOME/.config/vim/vimrc
 EOT
 
-cat << EOT > ~/.gvimrc
+cat <<EOT >~/.gvimrc
 source \$HOME/.config/vim/gvimrc
 EOT
